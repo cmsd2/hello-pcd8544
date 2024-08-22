e9108587e4ed080fe2d3833378928b41700bfa73
Chris Dawes <cmsd2@cantab.net>
2024-08-22 19:42:36 +0100

    it's working!!!

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
+
 use esp_backtrace as _;
 use esp_hal::clock::ClockControl;
 use esp_hal::delay::Delay;
-use esp_hal::gpio::Io;
-use esp_hal::ledc::{channel, timer, LSGlobalClkSource, Ledc, LowSpeed};
+use esp_hal::gpio::{self, Io, Level, Output};
 use esp_hal::peripherals::Peripherals;
 use esp_hal::prelude::*;
+use esp_hal::spi::master::Spi;
+use esp_hal::spi::{SpiBitOrder, SpiMode};
 use esp_hal::system::SystemControl;
+use pcd8544_spi::{Nokia5110, PCD8544};
 
 #[entry]
 fn main() -> ! {
@@ -19,34 +23,57 @@ fn main() -> ! {
     let delay = Delay::new(&clocks);
 
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-    //let mut led = Output::new(io.pins.gpio8, Level::High);
 
     esp_println::logger::init_logger_from_env();
 
-    let mut ledc = Ledc::new(peripherals.LEDC, &clocks);
-    ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
+    //let mut pcd_clk   = io.pins.gpio4..into_push_pull_output(&mut io.crh);
+    let sclk = io.pins.gpio4;
+    let mosi = io.pins.gpio5;
+    let cs0 = io.pins.gpio1;
+    let dc = Output::new(io.pins.gpio22, Level::High);
+    let rst = Output::new(io.pins.gpio12, Level::High);
 
-    let mut lstimer0 = ledc.get_timer::<LowSpeed>(timer::Number::Timer0);
-    lstimer0
-        .configure(timer::config::Config {
-            duty: timer::config::Duty::Duty5Bit,
-            clock_source: timer::LSClockSource::APBClk,
-            frequency: 4.kHz(),
-        })
-        .unwrap();
+    let spi = RefCell::new(
+        Spi::new(peripherals.SPI2, 100.kHz(), SpiMode::Mode0, &clocks)
+            .with_pins(
+                Some(sclk),
+                Some(mosi),
+                gpio::NO_PIN,
+                Some(cs0),
+            )
+            .with_bit_order(SpiBitOrder::MSBFirst, SpiBitOrder::MSBFirst),
+    );
 
-    let mut channel0 = ledc.get_channel(channel::Number::Channel0, io.pins.gpio8);
-    channel0
-        .configure(channel::config::Config {
-            timer: &lstimer0,
-            duty_pct: 50,
-            pin_config: channel::config::PinConfig::PushPull,
-        })
-        .unwrap();
+    let driver = PCD8544::new(&spi, dc, rst, delay).expect("Infallible cannot fail");
+    let mut display = Nokia5110::new(driver);
+
+    display.init().expect("init");
+
+    let mut ones = [0u8; 84];
+    let mut zeroes = [0u8; 84];
+    for i in 0..84 {
+        ones[i] = 0b11111111;
+        zeroes[i] = 0b00000000;
+    }
+
+    let mut lines = 0;
+    let mut filling_not_clearing = true;
 
     loop {
-        log::info!("Hello world!!");
+        log::info!("Hello 5110!!");
+
         //led.toggle();
         delay.delay(500.millis());
+
+        let line = if filling_not_clearing { &ones } else { &zeroes };
+
+        display.write_data(line).expect("write_data");
+
+        lines += 1;
+
+        if lines > 5 {
+            filling_not_clearing = !filling_not_clearing;
+            lines = 0;
+        }
     }
 }
